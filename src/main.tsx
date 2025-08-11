ff224612142baca1ad77eb7a497ee7c18557977f
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-08-11 10:33:44 -0700

    dark mode setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes/index.tsx'
+import { ThemeProvider } from './components/providers/theme.provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> 
+
     <RouterProvider router={router}/>
+    </ThemeProvider>
   </StrictMode>,
 )
