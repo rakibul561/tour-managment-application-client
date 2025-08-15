a84a2b9a123e51ae5b651f8cef2e65f0f0de3df4
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-08-15 09:06:46 -0700

    added tour type page

--- src/pages/admin/AddTour.tsx
+++ src/pages/admin/AddTour.tsx
@@ -1,12 +1,215 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useGetTourTypesQuery } from "@/redux/features/auth/tour/tour.api";
+import { useGetDivisionQuery } from "@/redux/features/division/division.api";
+import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
+
+const AddTour = () => { 
+      
+     const {data: tourTypeData} = useGetTourTypesQuery(undefined);
+     const {data: divisionData, isLoading:divisionLoading} = useGetDivisionQuery(undefined)
+
+     
+     const divisionOption = divisionData?.map((item: {_id: string, name:string}) => ({
+        value: item._id,
+        label: item.name
+     })) 
+      
+     console.log(divisionOption);
+     
+
+        
+    const form = useForm({
+        defaultValues: {
+            title: "",
+            division: "",
+            tourType: " ",
+            description: "",
+            location: "",
+            costFrom: "",
+            departureLocation: "",
+            arrivalLocation: "",
+        }
+    });
 
 
-const AddTour = () => {
+    const handleSubmit:SubmitHandler<FieldValues> = (data:any) =>{
+        console.log(data);
+        
+    }
+
+
+
     return (
-        <div>
+         <div className="w-full max-w-4xl mx-auto px-5 mt-16">
+      <Card>
+        <CardHeader>
+          <CardTitle>Add New Tour</CardTitle>
+          <CardDescription>Add a new tour to the system</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              id="add-tour-form"
+              className="space-y-5"
+              onSubmit={form.handleSubmit(handleSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Tour Title</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-            <h2> this is a tour page </h2>
+              <div className="flex gap-5">
+                <FormField
+                  control={form.control}
+                  name="location"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Location</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="costFrom"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Cost</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="flex gap-5">
+                <FormField
+                  control={form.control}
+                  name="departureLocation"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Departure Location</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="arrivalLocation"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Arrival Location</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="flex gap-5">
+                <FormField
+                  control={form.control}
+                  name="division"
+                  render={({ field }) => (
+                    <FormItem className="flex-1 ">
+                      <FormLabel>Division</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        disabled={divisionLoading}
+                        
+                      >
+                        <FormControl>
+                          <SelectTrigger className="w-full">
+                            <SelectValue />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {divisionOption?.map(
+                            (item: { label: string; value: string }) => (
+                              <SelectItem key={item.value} value={item.value}>
+                                {item.label}
+                              </SelectItem>
+                            )
+                          )}
+                        </SelectContent>
+                      </Select>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="tourType"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Tour Type</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger className="w-full">
+                            <SelectValue />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {}
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                
+
+
+
+
+
+
+
+
+
+              </div>
+
             
-        </div>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <Button type="submit" form="add-tour-form">
+            Create Tour
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
     );
 };
 
